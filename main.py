d33073c90684f9ca4c306385df8a85293752d011
HiDoYa <hiroyagojo@gmail.com>
2018-08-19 14:23:59 -0700

    Added mountain fractal

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ screen.fill(GRAY)
 def triangle(n, pos_1, pos_2, pos_3, color):
     # For cool flashing:
     #color = (color[0] - random.randint(10, 30), color[1] - random.randint(10, 30), color[2] - random.randint(10, 30))
-    color = (color[0], color[1] - 20, color[2] - 20)
+    color = (color[0], color[1] - 10, color[2] - 10)
     pygame.draw.polygon(screen, color, [pos_1, pos_2, pos_3])
 
     point_a = [(pos_1[0] + pos_2[0]) / 2, (pos_2[1] + pos_1[1]) / 2]
@@ -34,7 +34,7 @@ def triangle(n, pos_1, pos_2, pos_3, color):
     triangle(n - 1, point_b, point_c, pos_3, color)
 
 def tree(n, pos_1, angle, length, color):
-    color = (color[0], color[1] - 20, color[2] - 20)
+    color = (color[0], color[1] - 10, color[2] - 10)
 
     pos_2 = [pos_1[0] + math.cos(angle) * length, pos_1[1] + math.sin(angle) * length]
     pos_3 = [pos_2[0] + math.cos(angle / 2) * length, pos_2[1] + math.sin(angle / 2) * length]
@@ -47,12 +47,36 @@ def tree(n, pos_1, angle, length, color):
     if n == 0:
         return
 
-    tree(n - 1, pos_3, math.atan2(pos_3[1] - pos_2[1], pos_3[0] - pos_2[0]), length / 2, color)
-    tree(n - 1, pos_4, math.atan2(pos_4[1] - pos_2[1], pos_4[0] - pos_2[0]), length / 2, color)
+    tree(n - 1, pos_3, math.atan2(pos_3[1] - pos_2[1], pos_3[0] - pos_2[0]), length * 2 / 3, color)
+    tree(n - 1, pos_4, math.atan2(pos_4[1] - pos_2[1], pos_4[0] - pos_2[0]), length * 2 / 3, color)
 
-def mountain(n, pos_1, pos_2):
-    angle = math.atan2(pos_2[0] - pos_1[0], pos_2[1] - pos_1[1])
-    # TODO
+def mountain(n, pos_1, pos_2, color):
+    color = (color[0], color[1] - 10, color[2] - 10)
+
+    angle = math.atan2(pos_1[1] - pos_2[1], pos_2[0] - pos_1[0])
+    angle_new = angle + math.pi / 4
+
+    divisor = 2 + math.sqrt(2)
+    magnitude = math.sqrt(math.pow((pos_2[1] - pos_1[1]), 2) + math.pow((pos_2[0] - pos_1[0]), 2))
+    x = magnitude / divisor
+    
+    pos_a = [pos_1[0] + math.cos(angle) * x, pos_1[1] - math.sin(angle) * x]
+    pos_b = [pos_a[0] + math.cos(angle_new) * x, pos_a[1] - math.sin(angle_new) * x]
+    pos_c = [pos_2[0] - math.cos(angle) * x, pos_2[1] + math.sin(angle) * x]
+    
+    if n == 0:
+        pygame.draw.line(screen, color, pos_1, pos_a)
+        pygame.draw.line(screen, color, pos_a, pos_b)
+        pygame.draw.line(screen, color, pos_b, pos_c)
+        pygame.draw.line(screen, color, pos_c, pos_2)
+        return
+
+    mountain(n - 1, pos_1, pos_a, color)
+    mountain(n - 1, pos_a, pos_b, color)
+    mountain(n - 1, pos_b, pos_c, color)
+    mountain(n - 1, pos_c, pos_2, color)
+
+n = 3
 
 while not done:
     for event in pygame.event.get():
@@ -62,15 +86,29 @@ while not done:
     pressed = pygame.key.get_pressed()
 
     time_pass += 1
+    if pressed[pygame.K_UP] and time_pass > 10:
+        time_pass = 0
+        n += 1
+        print(n)
+    if pressed[pygame.K_DOWN] and time_pass > 10:
+        time_pass = 0
+        n -= 1
+        print(n)
+
 
     if pressed[pygame.K_1] and time_pass > 20:
         screen.fill(GRAY)
-        triangle(6, [640, 120], [300, 600], [980, 600], WHITE)
+        triangle(10, [640, 120], [300, 600], [980, 600], WHITE)
         time_pass = 0
     if pressed[pygame.K_2] and time_pass > 20:
         screen.fill(GRAY)
-        tree(10, [640, 0], math.pi / 2, 200, WHITE)
+        tree(15, [640, 0], math.pi / 2, 140, WHITE)
         time_pass = 0
+    if pressed[pygame.K_3] and time_pass > 20:
+        screen.fill(GRAY)
+        mountain(6, [50, 500], [1230, 500], WHITE)
+        time_pass = 0
+
 
     pygame.display.flip()
 
